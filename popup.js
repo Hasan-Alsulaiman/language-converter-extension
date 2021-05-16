29bb079ac89307ce2e61b7a84ccbd0ead8fe3dea
Hasan-Alsulaiman <careerforma@gmail.com>
2021-05-16 18:07:32 +0300

    add sha256

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.getElementById("inputText").addEventListener("click", getText);
+    function getText(){
+        var x = document.getElementById("inputText").value;
+        console.log(x);
+    }
