f44001dd2378618a641e7a06021502398b085660
Hasan-Alsulaiman <careerforma@gmail.com>
2021-05-16 18:42:02 +0300

    fail add event listener

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 document.getElementById("inputText").addEventListener("click", getText);
-    function getText(){
-        console.log("hello there")
-        var x = document.getElementById("inputText").value;
-        console.log("text is: ",x);
-    }
+
+function getText(){ 
+    var x = document.getElementById("inputText").value;
+    console.log("text is: ",x)
+};
