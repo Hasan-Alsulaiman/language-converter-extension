09cde066101a9334b9af22a672efa30fd1deed50
Hasan-Alsulaiman <careerforma@gmail.com>
2021-05-16 22:36:46 +0300

    load json

--- popup.js
+++ popup.js
@@ -2,7 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     var checkButton = document.getElementById('submit');
     checkButton.addEventListener('click', function() {
     const x = document.getElementById("inputText").value;
-     alert( x);
+
+    const url = chrome.runtime.getURL('json/json.json');
+    fetch(url)
+        .then((response) => response.json()) //assuming file contains json
+        .then((json) => alert(json.a));
+
+     
 
     }, false);
   }, false);
